7614fe7d6eb843b9edd5eddfb095fe6bca5841ed
Shannon Lance <shannon.lance@sap.com>
2023-11-17 15:44:29 -0800

    completed arrays 101 in-place operations

---
+++ src/main/java/com/shannonmlance/arrays101/inPlaceOperations/SortArrayByParity.java
@@ -0,0 +1,56 @@
+package main.java.com.shannonmlance.arrays101.inPlaceOperations;
+
+public class SortArrayByParity {
+
+/*    Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+Return any array that satisfies this condition.
+
+Example 1:
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+1 <= nums.length <= 5000
+0 <= nums[i] <= 5000
+ */
+
+    public static int[] sortArrayByParity1(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] % 2 == 0) {
+                i++;
+            } else if (nums[j] % 2 == 1) {
+                j--;
+            } else {
+                nums = swap(nums, i, j);
+            }
+        }
+        return nums;
+    }
+
+    public static int[] sortArrayByParity2(int[] nums) {
+        int pointer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                if (i != pointer) {
+                    nums = swap(nums, i, pointer);
+                }
+                pointer++;
+            }
+        }
+        return nums;
+    }
+
+    public static int[] swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        return nums;
+    }
+}
